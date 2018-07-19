9e1439aca140c6f367447b68a1fb8f533a9b5df4
Yaroslav Moroz <yaroslav.moroz91@outlook.com>
2018-07-19 22:01:08 +0300

    compare time

--- compileIfChanged.go
+++ compileIfChanged.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("ok")
+	file1, _ := os.Lstat(os.Args[1])
+	file2, _ := os.Lstat(os.Args[2])
+	if file1.ModTime().After(file2.ModTime()) {
+		fmt.Println(file1.Name() + " is modified after " + file2.Name())
+	}
 }
